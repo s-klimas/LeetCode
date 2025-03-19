1f58fa4193c8cd4931417f60043bcde8a4f233ff
Sebastian Klimas <sebastianklimas@op.pl>
2025-03-19 19:51:18 +0100

    Solution for 20. Valid Parentheses (changed line separator)

--- src/main/java/pl/sebastianklimas/lc20ValidParentheses/Solution.java
+++ src/main/java/pl/sebastianklimas/lc20ValidParentheses/Solution.java
@@ -1,1 +1,30 @@
-package pl.sebastianklimas.lc20ValidParentheses;import java.util.Map;import java.util.Stack;public class Solution {    public boolean isValid(String s) {        Stack<String> expected = new Stack<>();        Map<Character, String> map = Map.of(                '(', ")",                '[', "]",                '{', "}"        );        for (int i = 0; i < s.length(); i++) {            char c = s.charAt(i);            if (c == '(' || c == '{' || c == '[') {                expected.push(map.get(c));            } else if (c == ')' || c == '}' || c == ']') {                if (!expected.isEmpty() && expected.peek().equals(String.valueOf(c))) {                    expected.pop();                } else {                    return false;                }            } else {                return false;            }        }        return expected.isEmpty();    }}
+package pl.sebastianklimas.lc20ValidParentheses;
+
+import java.util.Map;
+import java.util.Stack;
+
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<String> expected = new Stack<>();
+        Map<Character, String> map = Map.of(
+                '(', ")",
+                '[', "]",
+                '{', "}"
+        );
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                expected.push(map.get(c));
+            } else if (c == ')' || c == '}' || c == ']') {
+                if (!expected.isEmpty() && expected.peek().equals(String.valueOf(c))) {
+                    expected.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+        return expected.isEmpty();
+    }
+}
